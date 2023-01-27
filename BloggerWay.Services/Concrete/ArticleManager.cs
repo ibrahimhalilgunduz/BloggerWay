37e6ead18990ff70325d2b95ade9a731df963130
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:06 +0300

    Article Manager  sınıfı  yazıldı

---
+++ BloggerWay.Services/Concrete/ArticleManager.cs
@@ -0,0 +1,124 @@
+﻿using BloggerWay.Data.Abstract;
+using BloggerWay.Entities.Dtos;
+using BloggerWay.Services.Abstract;
+using BloggerWay.Shared.Utilities.Results.Abstract;
+using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using BloggerWay.Shared.Utilities.Results.Concrete;
+using System;
+using System.Threading.Tasks;
+
+namespace BloggerWay.Services.Concrete
+{
+    public class ArticleManager : IArticleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ArticleManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public async Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Delete(int articleId, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<ArticleDto>> Get(int articleId)
+        {
+            var articles = await _unitOfWork.Articles.GetAsync(a => a.Id == articleId, a => a.User, a => a.Category);
+            if (articles != null)
+            {
+                return new DataResult<ArticleDto>(ResultStatus.Success, new ArticleDto
+                {
+                    Article = articles,
+                    ResultStatus = ResultStatus.Success,
+                });
+            }
+            return new DataResult<ArticleDto>(ResultStatus.Error, "Böyle bir Makale bulunamadı.", null);
+
+
+
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAll()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(null, a => a.User, a => a.Category);
+            if (articles.Count > -1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı.", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId)
+        {
+            var result = await _unitOfWork.Categories.AnyAsync(c => c.Id == categoryId);
+            if (result)
+            {
+                var articles = await _unitOfWork.Articles.GetAllAsync(a => a.CategoryId == categoryId, a => a.IsDeleted == false, a => a.IsActive, a => a.IsActive, a => a.User, a => a.Category);
+                if (articles.Count > -1)
+                {
+                    return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                    {
+                        Articles = articles,
+                        ResultStatus = ResultStatus.Success
+                    });
+
+                }
+                return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı.", null);
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Böyle bir Kategori bulunamadı.", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByNonDeleted()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(a => a.IsDeleted == false, a => a.User, a => a.Category);
+            if (articles.Count > -1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı.", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(a => a.IsDeleted == false, a => a.IsActive, a => a.User, a => a.Category);
+            if (articles.Count > -1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı.", null);
+        }
+
+        public Task<IResult> HardDelete(int articleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
