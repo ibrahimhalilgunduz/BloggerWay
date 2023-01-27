6aa83a154486f0569af155dcd1ccb0997ec0f3ca
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:05 +0300

    ArticleDto, ArticleListDto ve DtoGetBase Sınıfları Oluşturuldu

--- BloggerWay.Services/Abstract/ICategoryService.cs
+++ BloggerWay.Services/Abstract/ICategoryService.cs
@@ -10,10 +10,12 @@ namespace BloggerWay.Services.Abstract
     {
         Task<IDataResult<Category>> Get(int categoryId);
         Task<IDataResult<IList<Category>>> GetAll();
-        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
-        Task<IResult> Update(CategoryUpdateDto categoryAddDto, string modifiedByName);
+        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
 
-        Task<IResult> Delete(int categoryId);
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+
+        Task<IResult> Delete(int categoryId, string modifiedByName);
         Task<IResult> HardDelete(int categoryId);
     }
 }
