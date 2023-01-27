5e9b9db29ff04fc6b92a0a344acf8e656fcd2dec
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:04 +0300

    RoleMap yazıldı ilk degerler girildi.

---
+++ BloggerWay.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -0,0 +1,41 @@
+﻿using BloggerWay.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace BloggerWay.Data.Concrete.EntityFramework.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Id).ValueGeneratedOnAdd();
+            builder.Property(r => r.Name).HasMaxLength(30);
+            builder.Property(r => r.Description).IsRequired(true).HasMaxLength(250);
+            builder.Property(r => r.CreatedByName).IsRequired(true).HasMaxLength(50);
+            builder.Property(r => r.ModifiedByName).IsRequired(true).HasMaxLength(50);
+            builder.Property(r => r.CreatedDate).IsRequired(true);
+            builder.Property(r => r.ModifiedDate).IsRequired(true);
+            builder.Property(r => r.IsActive).IsRequired(true);
+            builder.Property(r => r.IsDeleted).IsRequired(true);
+            builder.Property(r => r.Note).HasMaxLength(500);
+            builder.ToTable("Roles");
+            builder.HasData(new Role
+            {
+                Id = 1,
+                Name = "Admin",
+                Description = "Admin Rolü, tüm haklara sahiptir",
+                IsActive = true,
+                IsDeleted = false,
+                CreatedByName = "InitialCreate",
+                CreatedDate = DateTime.Now,
+                ModifiedByName = "InıtialCreate",
+                ModifiedDate = DateTime.Now,
+                Note = "Admin Rolüdür"
+
+            });
+
+        }
+    }
+}
