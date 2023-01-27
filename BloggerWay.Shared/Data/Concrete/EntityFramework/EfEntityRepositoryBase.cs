a76bc638379a42e1b054cadf0589e9c6c4a8c341
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:50:10 +0300

    EfEntityRepositoryBase için GetAsyncV2 & GetAllAsyncV2 Metodları yazıldı.

--- BloggerWay.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ BloggerWay.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace BloggerWay.Shared.Data.Concrete.EntityFramework
 {
     public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
-     where TEntity : class, IEntity, new()
+    where TEntity : class, IEntity, new()
     {
         protected readonly DbContext _context;
 
@@ -19,6 +19,29 @@ namespace BloggerWay.Shared.Data.Concrete.EntityFramework
         {
             _context = context;
         }
+
+        public async Task<IList<TEntity>> GetAllAsyncV2(IList<Expression<Func<TEntity, bool>>> predicates, IList<Expression<Func<TEntity, object>>> includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate); // isActive==false && isDeleted==true
+                }
+            }
+
+            if (includeProperties != null && includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
         public async Task<TEntity> AddAsync(TEntity entity)
         {
             await _context.Set<TEntity>().AddAsync(entity);
@@ -65,6 +88,28 @@ namespace BloggerWay.Shared.Data.Concrete.EntityFramework
         public async Task DeleteAsync(TEntity entity)
         {
             await Task.Run(() => { _context.Set<TEntity>().Remove(entity); });
+        }
+
+        public async Task<TEntity> GetAsyncV2(IList<Expression<Func<TEntity, bool>>> predicates, IList<Expression<Func<TEntity, object>>> includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate); // isActive==false && isDeleted==true
+                }
+            }
+
+            if (includeProperties != null && includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().SingleOrDefaultAsync();
         }
 
         public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
