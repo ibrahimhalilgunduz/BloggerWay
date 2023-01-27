c44d33b3768d701f7c2dd342b29864cfdcc304ff
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:46 +0300

    ImageUpload Metodu ile Resim Yükleme kısmı yazıldı DateTime Extension metodu kullanıldı

--- BloggerWay.MVC/Areas/Admin/Controllers/UserController.cs
+++ BloggerWay.MVC/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 ﻿using BloggerWay.Entities.Concrete;
 using BloggerWay.Entities.Dtos;
+using BloggerWay.Shared.Utilities.Extensions;
 using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BloggerWay.MVC.Areas.Admin.Controllers
@@ -12,10 +16,12 @@ namespace BloggerWay.MVC.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env)
         {
             _userManager = userManager;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -31,6 +37,22 @@ namespace BloggerWay.MVC.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
+        }
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwwroot = _env.WebRootPath;
+
+            string fileExtension = Path.GetExtension(userAddDto.Picture.FileName);
+            DateTime dateTime = DateTime.Now;
+            //AliVeli_578_1_44_15_4_12_2022.png
+            string fileName = $"{userAddDto.UserName}_{dateTime.FullDateAndTimeStringWithUnderscore()}_{fileExtension}";
+            var path = Path.Combine($"{wwwroot}/img", fileName);
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await userAddDto.Picture.CopyToAsync(stream);
+            }
+            return fileName;//AliVeli_578_1_44_15_4_12_2022.png " "~/img/user.picture"
+
         }
     }
 }
