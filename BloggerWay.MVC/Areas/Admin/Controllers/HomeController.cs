01f96cf0374c9b94a81ab883fde4809b263c14a6
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:51 +0300

    DashboardViewModel ve Home/Index Action oluşturuldu.

--- BloggerWay.MVC/Areas/Admin/Controllers/HomeController.cs
+++ BloggerWay.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BloggerWay.Entities.Concrete;
+using BloggerWay.Services.Abstract;
+using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace BloggerWay.MVC.Areas.Admin.Controllers
 {
@@ -8,9 +14,40 @@ namespace BloggerWay.MVC.Areas.Admin.Controllers
     [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(ICategoryService categoryService, IArticleService articleService, ICommentService commentService, UserManager<User> userManager)
         {
-            return View();
+            _categoryService = categoryService;
+            _articleService = articleService;
+            _commentService = commentService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categoriesCountResult = await _categoryService.CountByIsDeleted();
+            var articlesCountResult = await _articleService.CountByIsDeleted();
+            var commentsCountResult = await _commentService.CountByIsDeleted();
+            var usersCount = await _userManager.Users.CountAsync();
+            var articlesResult = await _articleService.GetAll();
+            if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && usersCount > -1 && articlesResult.ResultStatus == ResultStatus.Success)
+            {
+                return View(new DashboardViewModel
+                {
+                    CategoriesCount = categoriesCountResult.Data,
+                    ArticlesCount = articlesCountResult.Data,
+                    CommentsCount = commentsCountResult.Data,
+                    UsersCount = usersCount,
+                    Articles = articlesResult.Data
+                });
+            }
+
+            return NotFound();
+
         }
     }
 }
---
+++ BloggerWay.MVC/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BloggerWay.Entities.Dtos;
+
+namespace BloggerWay.MVC.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int CategoriesCount { get; set; }
+        public int ArticlesCount { get; set; }//aktif ve silinmemiş makaleler
+        public int CommentsCount { get; set; }
+        public int UsersCount { get; set; }
+        public ArticleListDto Articles { get; set; }//tum makaleler
+
+    }
+}
