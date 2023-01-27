030b55168035724cddc14f08d063e800f768bc09
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:02 +0300

    Kategori  sınıfı oluşturuldu.

---
+++ BloggerWay.Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using BloggerWay.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace BloggerWay.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
