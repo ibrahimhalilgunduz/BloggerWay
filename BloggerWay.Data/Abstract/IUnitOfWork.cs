053ac6d48f70c0f87e3b4dd27c756b73136a0c5a
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:04 +0300

    IUnitOfWork arayüzü oluşturuldu.

---
+++ BloggerWay.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BloggerWay.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveAsync();
+
+    }
+}
