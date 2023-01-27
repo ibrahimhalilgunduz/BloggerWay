e103acd751f3c1d4a9ecfc29fd574be1390d0f2a
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:50:03 +0300

    Yorum Ekleme işlemleri backend kısımları kodlandı.

---
+++ BloggerWay.MVC/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BloggerWay.Entities.Dtos;
+
+namespace BloggerWay.MVC.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
