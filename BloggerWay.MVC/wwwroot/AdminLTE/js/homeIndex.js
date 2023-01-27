5d1413b8c550a45bca2fb5e88d5061056ef89583
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:50:11 +0300

    Chart.js ile Makalelerin Okunma Sayıları Görüntülendi.

--- BloggerWay.MVC/wwwroot/AdminLTE/js/homeIndex.js
+++ BloggerWay.MVC/wwwroot/AdminLTE/js/homeIndex.js
@@ -38,60 +38,39 @@
     // DataTable
 
     // Chart.js
-    const categories = [
-        {
-            name: 'C#',
-            viewCount: '24500'
-        },
-        {
-            name: 'C++',
-            viewCount: '77000'
-        },
-        {
-            name: 'Javascript',
-            viewCount: '57800'
-        },
-        {
-            name: 'Dart',
-            viewCount: '2250'
-        },
-        {
-            name: 'PHP',
-            viewCount: '123000'
-        },
-        {
-            name: 'Typescript',
-            viewCount: '18999'
-        }
-    ];
 
+    $.get('/Admin/Article/GetAllByViewCount/?isAscending=false&takeSize=10',
+        function (data) {
+            const articleResult = jQuery.parseJSON(data);
 
-    let viewCountContext = $('#viewCountChart');
+            let viewCountContext = $('#viewCountChart');
 
-    let viewCountChart = new Chart(viewCountContext,
-        {
-            type: 'bar',
-            data: {
-                labels: categories.map(category => category.name),
-                datasets: [
-                    {
-                        label: 'Okunma Sayısı',
-                        data: categories.map(category => category.viewCount),
-                        backgroundColor: ['#845ec2', '#ffc75f', '#f9f871', '#ff5e78', '#a4ebf3'],
-                        hoverBorderWidth: 4,
-                        hoverBorderColor: 'black'
-                    }]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        labels: {
-                            font: {
-                                size: 18
+            let viewCountChart = new Chart(viewCountContext,
+                {
+                    type: 'bar',
+                    data: {
+                        labels: articleResult.$values.map(article => article.Title),
+                        datasets: [
+                            {
+                                label: 'Okunma Sayısı',
+                                data: articleResult.$values.map(article => article.ViewCount),
+                                backgroundColor: '#fb3640',
+                                hoverBorderWidth: 4,
+                                hoverBorderColor: 'black'
+                            }]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    font: {
+                                        size: 18
+                                    }
+                                }
                             }
                         }
                     }
-                }
-            }
+                });
         });
+
 });
