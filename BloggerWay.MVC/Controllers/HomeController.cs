4a4b84d2a55b8ea686bea749f0dfd0034bb12ea3
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:59 +0300

    Index Sayfası oluşturuldu ve Makaleler listelendi.

--- BloggerWay.MVC/Controllers/HomeController.cs
+++ BloggerWay.MVC/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 ﻿
+using BloggerWay.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BloggerWay.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IArticleService _articleService;
+
+        public HomeController(IArticleService articleService)
         {
-            return View();
+            _articleService = articleService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var articleListDto = await _articleService.GetAllByNonDeletedAndActiveAsync();
+            return View(articleListDto.Data);
         }
     }
 }
