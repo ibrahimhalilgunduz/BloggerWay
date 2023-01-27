43badd05f024445e03d02fa21ee7faf956e39b78
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:57 +0300

    [HttpGet] Assign (Rol Atama) Action'ı Oluşturuldu.

---
+++ BloggerWay.MVC/Areas/Admin/Models/UserRoleAssignAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BloggerWay.Entities.Dtos;
+
+namespace BloggerWay.MVC.Areas.Admin.Models
+{
+    public class UserRoleAssignAjaxViewModel
+    {
+        public UserRoleAssignDto UserRoleAssignDto { get; set; }
+        public string RoleAssignPartial { get; set; }
+        public UserDto UserDto { get; set; }
+    }
+}
