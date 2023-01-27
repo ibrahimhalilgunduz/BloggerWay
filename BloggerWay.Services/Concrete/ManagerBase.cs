861cf0f3e0d909161f6f20f3cb3387eca8dffea0
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:55 +0300

    ManagerBase Sınıfı Oluşturuldu.

---
+++ BloggerWay.Services/Concrete/ManagerBase.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BloggerWay.Data.Abstract;
+
+namespace BloggerWay.Services.Concrete
+{
+    public class ManagerBase
+    {
+        public ManagerBase(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+
+        protected IUnitOfWork UnitOfWork { get; }
+        protected IMapper Mapper { get; }
+
+    }
+}
