c64447632b4b3ee91e731d98567552f709ed4a96
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:52 +0300

    Article/Add Sayfası ve Styles Alanı(RenderSection) Oluşturuldu.

---
+++ BloggerWay.MVC/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,31 @@
+﻿using BloggerWay.Services.Abstract;
+using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BloggerWay.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _articleService.GetAllByNonDeletedAsync();
+            if (result.ResultStatus == ResultStatus.Success) return View(result.Data);
+            return NotFound();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
