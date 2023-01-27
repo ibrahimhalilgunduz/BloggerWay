53b1da54ed8043df503e3ea3391b88946935bf00
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:05 +0300

    IResult Arayüzü oluşturuldu.

---
+++ BloggerWay.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,12 @@
+﻿using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace BloggerWay.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; } // ResultStatus.Success // ResultStatus.Error
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
---
+++ BloggerWay.Shared/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggerWay.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success = 0,
+        Error = 1,
+        Warning = 2, // ResultStatus.Warning
+        Info = 3 // ResultStatus.Info
+    }
+}
