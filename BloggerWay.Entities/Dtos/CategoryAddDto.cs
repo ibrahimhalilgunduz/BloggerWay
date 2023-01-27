fa12d39150be7932f40c9af076fa4ccc67a9da64
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:05 +0300

    CategoryUpdateDto tamamlandı.

--- BloggerWay.Entities/Dtos/CategoryAddDto.cs
+++ BloggerWay.Entities/Dtos/CategoryAddDto.cs
@@ -7,21 +7,21 @@ namespace BloggerWay.Entities.Dtos
     {
         [DisplayName("Kategori Adı")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
-        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
         [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Name { get; set; }
         [DisplayName("Kategori Açıklaması")]
         [Required(ErrorMessage = "{0} boş geçilemez")]
-        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
         [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Description { get; set; }
         [DisplayName("Kategori Özel Not  Alanı")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
-        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
         [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Note { get; set; }
         [DisplayName("Aktif Mi?")]
-        [Required(ErrorMessage = "{0} boş geçilemez")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         public bool IsActive { get; set; }
 
     }
