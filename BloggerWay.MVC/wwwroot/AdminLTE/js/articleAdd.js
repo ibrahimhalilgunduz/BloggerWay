1aa118f8a4183d238d7b3ef771ac0d88e889fe5b
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:52 +0300

    Trumbowyg jQuery Text Editor kütüphanesi eklendi.

---
+++ BloggerWay.MVC/wwwroot/AdminLTE/js/articleAdd.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $('#text-editor').trumbowyg({
+        lang: 'tr',
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ],
+        plugins: {
+            colors: {
+                foreColorList: [
+                    'ff0000', '00ff00', '0000ff', '54e346','383838'
+                ],
+                backColorList: [
+                    '000', '333', '555'
+                ],
+                displayAsList: false
+            }
+        }
+    });
+});
