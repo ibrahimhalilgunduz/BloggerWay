aacb20d020a67c6843141842ca41ad19c69a8dd2
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:03 +0300

    BloggerWayContext oluşturuldu.

---
+++ BloggerWay.Data/Concrete/EntityFramework/Contexts/BloggerWayContext.cs
@@ -0,0 +1,22 @@
+﻿using BloggerWay.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggerWay.Data.Concrete.EntityFramework.Contexts
+{
+    public class BloggerWayContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=127.0.0.1;Port=5432;Database=BloggerWay;User Id=postgres;Password=123;");
+        }
+
+
+
+
+    }
+}
