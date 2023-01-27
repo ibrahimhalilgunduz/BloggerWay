78c9f188520bd9693825fe328a492c8fc9ae3bd1
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:07 +0300

    ajax için IEntityRepository,EfEntityRepository,ICategoryService,CategoryManager sınıflarında düzenleme yapıldı

--- BloggerWay.Shared/Data/Abstract/IEntityRepository.cs
+++ BloggerWay.Shared/Data/Abstract/IEntityRepository.cs
@@ -11,9 +11,9 @@ namespace BloggerWay.Shared.Data.Abstract
 
         Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);// kullanıcı = repository.getAsync 
         Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
-        Task AddAsync(T entity);
+        Task<T> AddAsync(T entity);
 
-        Task UpdateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
 
         Task DeleteAsync(T entity);
         Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
