ed1698729c869a53aa40bf47c66bb3d93915843d
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:06 +0300

    Category Controller, Index Action ve Index View kısımları oluşturuldu.

---
+++ BloggerWay.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using BloggerWay.Services.Abstract;
+using BloggerWay.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BloggerWay.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
