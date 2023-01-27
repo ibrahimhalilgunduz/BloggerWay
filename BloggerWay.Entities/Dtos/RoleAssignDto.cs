5977da014ec21fe90705e9ba430a47807e0f0a33
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:56 +0300

    UserRoleAssignDto ve RoleAssignDto Oluşturuldu.

---
+++ BloggerWay.Entities/Dtos/RoleAssignDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BloggerWay.Entities.Dtos
+{
+    public class RoleAssignDto
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool HasRole { get; set; }
+    }
+}
