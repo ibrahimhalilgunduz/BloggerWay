6c94b3fe3c653155e877eac40805af85e2df8983
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:05 +0300

    CategoryAddDto Sınıfı Tamamlandı

--- BloggerWay.Entities/Dtos/CategoryAddDto.cs
+++ BloggerWay.Entities/Dtos/CategoryAddDto.cs
@@ -1,8 +1,28 @@
-﻿namespace BloggerWay.Entities.Dtos
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggerWay.Entities.Dtos
 {
     public class CategoryAddDto
     {
-
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması")]
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not  Alanı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olamaz")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsActive { get; set; }
 
     }
 }
