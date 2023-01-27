1323ab25749e7c70a3f35a114d77bee86c5f91a1
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:50:09 +0300

    Genel Ayarlar ve E posta Ayarları  sayfaları yazıldı.

--- BloggerWay.MVC/Startup.cs
+++ BloggerWay.MVC/Startup.cs
@@ -33,7 +33,8 @@ namespace BloggerWay.MVC
             services.Configure<WebsiteInfo>(Configuration.GetSection("WebsiteInfo"));
             services.Configure<SmtpSettings>(Configuration.GetSection("SmtpSettings"));
             services.ConfigureWritable<AboutUsPageInfo>(Configuration.GetSection("AboutUsPageInfo"));
-
+            services.ConfigureWritable<WebsiteInfo>(Configuration.GetSection("WebsiteInfo"));
+            services.ConfigureWritable<SmtpSettings>(Configuration.GetSection("SmtpSettings"));
             services.AddControllersWithViews(options =>
             {
                 options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Bu alan boþ geçilmemelidir.");
