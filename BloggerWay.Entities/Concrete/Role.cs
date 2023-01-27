640299d0d8f769ab0934b440754596270e8af46c
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:03 +0300

    Role sınıfı oluşturuldu.

---
+++ BloggerWay.Entities/Concrete/Role.cs
@@ -0,0 +1,12 @@
+﻿using BloggerWay.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace BloggerWay.Entities.Concrete
+{
+    public class Role : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
