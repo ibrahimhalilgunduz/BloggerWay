f46d1f7c3959ac268ab8f53a363da2d17856db59
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:48:03 +0300

    Comment sınıfı oluşturuldu

---
+++ BloggerWay.Entities/Concrete/Comment.cs
@@ -0,0 +1,12 @@
+﻿using BloggerWay.Shared.Entities.Abstract;
+
+namespace BloggerWay.Entities.Concrete
+{
+    public class Comment : EntityBase, IEntity
+    {
+        public string Text { get; set; }
+        public int ArticleId { get; set; }
+        public Article Article { get; set; }
+
+    }
+}
