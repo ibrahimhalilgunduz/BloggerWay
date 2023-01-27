dbe103ceaf0d0f93690c9a86fe5171a91f1f8082
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:52 +0300

    wwwroot klasoru ve asenkron metodların isimleri için düzeltme yapıldı.

--- BloggerWay.MVC/Areas/Admin/Controllers/HomeController.cs
+++ BloggerWay.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BloggerWay.Entities.Concrete;
+using BloggerWay.MVC.Areas.Admin.Models;
 using BloggerWay.Services.Abstract;
 using BloggerWay.Shared.Utilities.Results.ComplexTypes;
 using Microsoft.AspNetCore.Authorization;
@@ -29,9 +30,9 @@ namespace BloggerWay.MVC.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var categoriesCountResult = await _categoryService.CountByIsDeleted();
-            var articlesCountResult = await _articleService.CountByIsDeleted();
-            var commentsCountResult = await _commentService.CountByIsDeleted();
+            var categoriesCountResult = await _categoryService.CountByNonDeleted();
+            var articlesCountResult = await _articleService.CountByNonDeleted();
+            var commentsCountResult = await _commentService.CountByNonDeleted();
             var usersCount = await _userManager.Users.CountAsync();
             var articlesResult = await _articleService.GetAll();
             if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && usersCount > -1 && articlesResult.ResultStatus == ResultStatus.Success)
