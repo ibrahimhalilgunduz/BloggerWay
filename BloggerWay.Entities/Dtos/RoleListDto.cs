944d2d2309d14287ea33fdb178fa937665453f3d
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:56 +0300

    RoleController  ile  Gerekli Action , RoleListDto, Index ,View ve  Js Dosyaları oluşturuldu.

---
+++ BloggerWay.Entities/Dtos/RoleListDto.cs
@@ -0,0 +1,10 @@
+﻿using BloggerWay.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace BloggerWay.Entities.Dtos
+{
+    public class RoleListDto
+    {
+        public IList<Role> Roles { get; set; }
+    }
+}
