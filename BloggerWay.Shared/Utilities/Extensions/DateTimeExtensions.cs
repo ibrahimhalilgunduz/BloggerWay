be9eb7a8faf88540d0cb3f36a769806e88b815e9
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:45 +0300

    Resim Yükleme işlemi için DateTime Extension Metodu eklendi

---
+++ BloggerWay.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BloggerWay.Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime datetime)
+        {
+            return $"{datetime.Millisecond} {datetime.Second} {datetime.Minute} {datetime.Hour} {datetime.Day} {datetime.Month} {datetime.Year}";
+
+        }
+    }
+}
