d03dd6efc9679277a86574e9649c741b5b7d1cc9
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:49:55 +0300

    CommentController, Index View ve commentIndex.js oluşturuldu.

---
+++ BloggerWay.MVC/Areas/Admin/Models/CommentUpdateAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BloggerWay.Entities.Dtos;
+
+namespace BloggerWay.MVC.Areas.Admin.Models
+{
+    public class CommentUpdateAjaxViewModel
+    {
+        public CommentUpdateDto CommentUpdateDto { get; set; }
+        public string CommentUpdatePartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
