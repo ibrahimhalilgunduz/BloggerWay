ffab02fe896659298c5a878df8fb906106ad7779
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-01-27 20:50:06 +0300

    NLog  Kuruldu.

--- BloggerWay.MVC/Program.cs
+++ BloggerWay.MVC/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NLog.Web;
 
 namespace BloggerWay.MVC
 {
@@ -17,10 +13,13 @@ namespace BloggerWay.MVC
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+             Host.CreateDefaultBuilder(args)
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseStartup<Startup>();
+                 }).ConfigureLogging(logging =>
+                 {
+                     logging.ClearProviders();
+                 }).UseNLog();
     }
 }
